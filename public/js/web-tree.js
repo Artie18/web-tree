168d185f14ce07bdb1fdbb0a1a2637270a73e19e
Artyom Fedenko <artyomfedenko@hotmail.com>
2015-04-24 18:08:42 +0300

    Added Project Folder

--- public/js/web-tree.js
+++ public/js/web-tree.js
@@ -14,6 +14,8 @@ var WebTree = function(type, opts) {
 }
 
 var WebTreeNative = function (opts) {
+  // Define self to use inside of functions
+  self = this;
   // URL to fetch data from
   this.fetchBackendUrl = opts.postUrl || opts.url;
   // URL to post data to
@@ -23,6 +25,9 @@ var WebTreeNative = function (opts) {
   this.buildDataFromDom = false; //opts.fromDom || false;
   // If We are building from DOM, we need Node from wich to choose
   this.nodeWhereDomDataIs = opts.dataDomElement;
+  // If we need to listen for events to drag and drop our site map elements
+  this.listen = opts.listen || true;
+
   if(this.buildDataFromDom) {
     this.fetchDomData();
   }
@@ -32,19 +37,46 @@ var WebTreeNative = function (opts) {
     // TODO: Implement
   };
 
+  this.onDragStart = function (e) {
+    e.dataTransfer.setData('id', e.toElement.id);
+  }
+
+  this.onDragOver = function (e) {
+    e.preventDefault();
+  }
+
+  this.onDrop = function (e) {
+    e.preventDefault();
+    var el = e.toElement;
+    var elToDrop = document.getElementById(e.dataTransfer.getData('id'));
+    if(el.className == "parent") {
+      el.firstElementChild.appendChild(elToDrop);
+    } else {
+      var _ul = document.createElement('ul');
+      _ul.appendChild(elToDrop);
+      el.appendChild(_ul);
+    }
+  }
+
+  // This function is renders basic DOM;
   this.render = function (node, data) {
     // Clean vars to clone node from
     var ul = document.createElement('ul');
     var li = document.createElement('li');
 
-    var mainUl = ul.cloneNode(true);
-
+    // Recursive function to build tree
     var buildTreePart = function (node, data) {
       var _ul = ul.cloneNode(true);
       for(var i = 0; i < data.length; i++) {
         var _li = li.cloneNode(true);
+        _li.draggable = true;
+        _li.id = data[i].id;
+        _li.ondragstart = self.onDragStart;
+        _li.ondragover = self.onDragOver;
+        _li.ondrop = self.onDrop;
         _li.textContent = data[i].name;
         if(Array.isArray(data[i].children)) {
+          _li.className = "parent";
           _li = buildTreePart(_li, data[i].children);
         }
         _ul.appendChild(_li);
@@ -52,7 +84,14 @@ var WebTreeNative = function (opts) {
       node.appendChild(_ul);
       return node;
     }
-
-    buildTreePart(node, data);
+    var _ul = ul.cloneNode(true),
+        _li = li.cloneNode(true);
+    _li.ondragover = self.onDragOver;
+    _li.ondrop = self.onDrop;
+    _li.textContent = 'Project 1';
+    node.appendChild(_ul.appendChild(_li));
+    buildTreePart(_li, data);
   };
+
+
 }
